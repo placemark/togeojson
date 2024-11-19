d074b214f207c8d1501424099f16855d0b62d549
Tom MacWright <tom@macwright.com>
2024-11-19 17:37:53 -0500

    Look for opencpn extensions to GPX
    - Refs #128

--- lib/gpx/properties.ts
+++ lib/gpx/properties.ts
@@ -1,36 +1,38 @@
 import { $, getMulti, nodeVal } from "../shared";
 
 export function extractProperties(node: Element) {
-  const properties = getMulti(node, [
-    "name",
-    "cmt",
-    "desc",
-    "type",
-    "time",
-    "keywords",
-  ]);
+	const properties = getMulti(node, [
+		"name",
+		"cmt",
+		"desc",
+		"type",
+		"time",
+		"keywords",
+	]);
 
-  const extensions = Array.from(
-    node.getElementsByTagNameNS(
-      "http://www.garmin.com/xmlschemas/GpxExtensions/v3",
-      "*"
-    )
-  );
-  for (const child of extensions) {
-    if (child.parentNode?.parentNode === node) {
-      properties[child.tagName.replace(":", "_")] = nodeVal(child);
-    }
-  }
+	const extensions = Array.from(
+		node.getElementsByTagNameNS(
+			"http://www.garmin.com/xmlschemas/GpxExtensions/v3",
+			"*",
+		),
+	).concat(
+		Array.from(node.getElementsByTagNameNS("http://www.opencpn.org", "*")),
+	);
+	for (const child of extensions) {
+		if (child.parentNode?.parentNode === node) {
+			properties[child.tagName.replace(":", "_")] = nodeVal(child);
+		}
+	}
 
-  const links = $(node, "link");
-  if (links.length) {
-    properties.links = links.map((link) =>
-      Object.assign(
-        { href: link.getAttribute("href") },
-        getMulti(link, ["text", "type"])
-      )
-    );
-  }
+	const links = $(node, "link");
+	if (links.length) {
+		properties.links = links.map((link) =>
+			Object.assign(
+				{ href: link.getAttribute("href") },
+				getMulti(link, ["text", "type"]),
+			),
+		);
+	}
 
-  return properties;
+	return properties;
 }
